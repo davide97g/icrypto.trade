865c66043533ac9216da266b710c5ac2b6904749
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-03-20 11:20:24 +0100

    assets mobile

--- apps/frontend/src/pages/Assets.vue
+++ apps/frontend/src/pages/Assets.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <h2>Assets</h2>
-    <a-list>
-      <a-list-item v-for="asset in assets" :key="asset.asset">
+    <a-list class="asset-list">
+      <a-list-item
+        v-for="asset in assets"
+        :key="asset.asset"
+        style="align-items: baseline"
+      >
         <a-list-item-meta title="Asset">
           <template #description>
             <a-button type="link" @click="openOrdersForSymbol(asset.asset)">
@@ -10,9 +14,18 @@
             </a-button>
           </template>
         </a-list-item-meta>
-        <a-list-item-meta title="Quantity">
+        <a-list-item-meta title="Free">
           <template #description>
-            {{ asset.free }} / {{ asset.locked }}
+            <a-tag color="green">
+              {{ asset.free }}
+            </a-tag>
+          </template>
+        </a-list-item-meta>
+        <a-list-item-meta title="Locked">
+          <template #description>
+            <a-tag color="red">
+              {{ asset.locked }}
+            </a-tag>
           </template>
         </a-list-item-meta>
       </a-list-item>
@@ -44,3 +57,10 @@ const openOrdersForSymbol = (symbol: string) => {
 
 getAssets();
 </script>
+
+<style lang="scss" scoped>
+.asset-list {
+  max-height: calc(100vh - 140px);
+  overflow: auto;
+}
+</style>
